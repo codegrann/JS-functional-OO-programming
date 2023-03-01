4f81690f43fa605acbdb9c39a281fe57857237a6
codegram <omondihills02@gmail.com>
2023-03-01 09:35:18 +0300

    .

--- OOP/ooprogramming.js
+++ OOP/ooprogramming.js
@@ -1,45 +1,33 @@
 // Task 1: Code a Person class
 class Person{
-    constructor(name="Tom", age=20, energy=100) {
+    constructor(name='Tom', age=20, energy=100) {
         this.name = name;
         this.age = age;
         this.energy = energy;
-        // this.sleep = function sleep(energy){
-            // energy = energy + 10;
-        //  }
+      
     }
-    sleep=function(energy) {
-       console.log(energy+10);
+    sleep(){
+        this.energy+=10;
     }
-    doSomethingFun=function(energy) {
-        return energy - 10;
+    doSomethingFun(){
+        this.energy-=10;
     }
 }
 
 // Task 2: Code a Worker class
 class Worker extends Person{
     constructor(name, age, energy, xp = 0, hourlyWage = 10) {
-        super();
+        super(name, energy, age);
         this.xp = xp;
         this.hourlyWage = hourlyWage;
     }
-    sleep() {
-        super.sleep();
-        // energy = energy-10;
-    }
-    doSomethingFun() {
-        super.doSomethingFun();
-    }
-
-    goToWork=function(xp){
-        xp=xp+10;
+   
+    goToWork(){
+        this.xp-=10;
     }
 }
 
-// let cow = new Person();
-// cow.doSomethingFun();
-// console.log(cow.energy);
-// console.log(cow.hourlyWage);
+
 
 
 
@@ -47,15 +35,9 @@ class Worker extends Person{
 
 // Task 3: Code an intern object, run methods
 function intern() {
-    let intern=new Worker();
-    intern.name="Bob";
-    intern.age=21;
-    intern.energy=110;
-    intern.xp=0;
-    intern.hourlyWage=10;
-    console.log(intern)
+    let intern=new Worker('Bob', 21, 110, 0, 10);
     intern.goToWork();
-    console.log(intern.xp);
+    return intern;
 }
 
 intern();
@@ -63,13 +45,8 @@ intern();
 
 // Task 4: Code a manager object, methods
 function manager() {
-    let manager=new Worker();
-    manager.name="Alice";
-    manager.age=30;
-    manager.energy=120;
-    manager.xp=100;
-    manager.hourlyWage=30;
-    console.log(manager.energy);
-    console.log(manager);
+    let manager=new Worker('Alice', 30, 120, 100, 30);
+    manager.doSomethingFun()
+    return manager;
 }
 manager();
