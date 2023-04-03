787fe6e2e9e5583c5b8eea116ca22c3739099a75
codegram <omondihills02@gmail.com>
2023-04-03 23:25:43 +0300

    .

--- leetCode.slns/medianOfSortedArrays.js
+++ leetCode.slns/medianOfSortedArrays.js
@@ -34,4 +34,21 @@ var arr1=[1,3];
 var arr2=[2];
 findMedianSortedArrays(arr1, arr2);
 
-console.log(Number.MAX_SAFE_INTEGER -1=== Number.MAX_SAFE_INTEGER +2)
+
+
+
+setTimeout(() => {
+  1  let isPrime = function isPrime(n) {
+    if (n <= 1) {
+      return false;
+  
+      // check from 2 to n-1
+      for (var i = 2; i < n; i++) {
+        if (n % i == 0) {
+          return false;
+        }
+      }
+      return true;
+    }
+    isPrime(3)
+}, timeout);
