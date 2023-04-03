bb4ee21e1f2a7c7ed2cd75159bc504f826ad1f8e
codegram <omondihills02@gmail.com>
2023-04-03 21:55:45 +0300

    .

--- leetCode.slns/medianOfSortedArrays.js
+++ leetCode.slns/medianOfSortedArrays.js
@@ -36,8 +36,9 @@ findMedianSortedArrays(arr1, arr2);
 
   let a = 1;
   function four() {
+    let a=2;
     if (true) {
-      let a = 4;
+      let a = 3;
   
     console.log(a); // prints '4'
   }
