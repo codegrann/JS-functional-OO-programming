e94bceecf6672cdb62b3adb3e1305a2bca0222f6
codegram <omondihills02@gmail.com>
2023-04-03 23:30:01 +0300

    checked if a number is prime

--- leetCode.slns/medianOfSortedArrays.js
+++ leetCode.slns/medianOfSortedArrays.js
@@ -37,18 +37,18 @@ findMedianSortedArrays(arr1, arr2);
 
 
 
-setTimeout(() => {
-  1  let isPrime = function isPrime(n) {
-    if (n <= 1) {
-      return false;
-  
-      // check from 2 to n-1
-      for (var i = 2; i < n; i++) {
-        if (n % i == 0) {
-          return false;
-        }
+ function isPrime(n) {
+  if (n <= 1) {
+    return false;
+  }
+    // check from 2 to n-1
+    for (var i = 2; i < n; i++) {
+      if (n % i == 0) {
+        console.log(false);
+        return false;
       }
-      return true;
     }
-    isPrime(3)
-}, timeout);
+    console.log(true);
+    return true;
+  }
+  isPrime(9)
