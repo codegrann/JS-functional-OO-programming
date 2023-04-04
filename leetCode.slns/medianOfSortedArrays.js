8a6930149b9e9ebb62124539009b0f53dd65ab61
codegram <omondihills02@gmail.com>
2023-04-04 15:27:27 +0300

    .

--- leetCode.slns/medianOfSortedArrays.js
+++ leetCode.slns/medianOfSortedArrays.js
@@ -37,18 +37,5 @@ findMedianSortedArrays(arr1, arr2);
 
 
 
- function isPrime(n) {
-  if (n <= 1) {
-    return false;
-  }
-    // check from 2 to n-1
-    for (var i = 2; i < n; i++) {
-      if (n % i == 0) {
-        console.log(false);
-        return false;
-      }
-    }
-    console.log(true);
-    return true;
-  }
-  isPrime(9)
+
+  'Red Dragon'.endsWith('Red');
