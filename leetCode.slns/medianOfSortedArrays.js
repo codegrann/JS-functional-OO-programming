1821850f434c97bc9e86862377750160b855b444
codegram <omondihills02@gmail.com>
2023-04-06 06:33:44 +0300

    commented a section of the ode

--- leetCode.slns/medianOfSortedArrays.js
+++ leetCode.slns/medianOfSortedArrays.js
@@ -1,6 +1,6 @@
 function findMedianSortedArrays(nums1, nums2) {
     if (nums1.length > nums2.length) {
-    
+      //Ensure second array is longer than the first
       [nums1, nums2] = [nums2, nums1]; 
     }
     
