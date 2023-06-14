c10205c1ef6f6120cba749424fd5b8094ad5d4bd
codegrann <omondihills02@gmail.com>
2023-06-14 06:56:42 +0300

    .

--- RegEx.js
+++ RegEx.js
@@ -54,5 +54,5 @@ console.log('ES\n6'.match(rd));
 
 
 let sentence="It @\n was a very chilly morning when I woke up, just to prepare for school. It happened the previous day that I missed 2 lectures so I had to be up early to cover up for the miss. A friend of mine informed me of some assignments given to them which were due that same day.";
-console.log(/^I/.test(sentence))
+console.log(/^/.test(sentence))
 
