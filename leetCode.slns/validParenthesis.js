a942c0e7cde92c9f25a495d55a185131668b29b4
codegrann <omondihills02@gmail.com>
2023-06-22 07:14:04 +0300

    .

--- leetCode.slns/validParenthesis.js
+++ leetCode.slns/validParenthesis.js
@@ -9,6 +9,11 @@ isValid("()");
 
 function doPower(){
     let num=3, pwr=2;
+    let ans=num;
+    for(let i=1; i<pwr; i++){
+        console.log(ans*=num)
+    }
+
     console.log(Math.pow(num, pwr))
 }
 doPower()
