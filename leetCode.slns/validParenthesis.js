fb164d2846312acb17ff014e0ecb9691832cec3b
codegrann <omondihills02@gmail.com>
2023-06-22 06:57:21 +0300

    .

--- leetCode.slns/validParenthesis.js
+++ leetCode.slns/validParenthesis.js
@@ -12,6 +12,12 @@ function checkAge() {
     // Using ?
     let isAllowed=age>18? true: confirm('Did parents allow you');
     console.log(isAllowed);
+
+    // Using ||
+    let allowed=age>18 || confirm('Did parents allow you');
+    console.log(allowed)
+
+    // Using  if
     if (age > 18) {
       console.log(true);
     } else {
