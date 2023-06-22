bb3c9e07091cb0d3ab71c10402c77de628eb90b7
codegrann <omondihills02@gmail.com>
2023-06-22 07:29:41 +0300

    .

--- leetCode.slns/validParenthesis.js
+++ leetCode.slns/validParenthesis.js
@@ -7,3 +7,11 @@ function isValid(s) {
 isValid("()");
 
 
+function sayHi() {   // (1) create
+    console.log( "Hello" );
+  }
+  
+  let  func =sayHi;
+  
+  func(); // Hello     // (3) run the copy (it works)!
+  sayHi();
