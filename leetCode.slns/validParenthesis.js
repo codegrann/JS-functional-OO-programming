e74689c1f1dff0e48ad6d89e9cffab91b77fa51f
codegrann <omondihills02@gmail.com>
2023-06-22 06:20:16 +0300

    .

--- leetCode.slns/validParenthesis.js
+++ leetCode.slns/validParenthesis.js
@@ -1,3 +1,17 @@
 function isValid(s){
-    
+    if(s){
+
+        console.log(s)
+    }
 }
+
+isValid('()')
+
+
+function show(){
+    if(false){
+        return;
+    }
+    console.log('hi')
+}
+show()
