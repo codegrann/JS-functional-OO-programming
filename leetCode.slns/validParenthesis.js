712851dbc75f871a7d5045b1e9aa1b05489f5d45
codegrann <omondihills02@gmail.com>
2023-07-03 08:18:30 +0300

    .

--- leetCode.slns/validParenthesis.js
+++ leetCode.slns/validParenthesis.js
@@ -6,3 +6,14 @@ function isValid(s) {
 
 isValid("()");
 
+let user={
+  name: 'jango',
+}
+let user2=user;
+
+console.log(user);
+console.log(user2);
+
+user=null;
+console.log(user);
+console.log(user2);
