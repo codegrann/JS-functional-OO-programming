40405b791da23a7bc28aaf429a5211e36e6c0de0
codegrann <omondihills02@gmail.com>
2023-09-26 21:37:40 +0300

    .

---
+++ unaryVsNumber.js
@@ -0,0 +1,17 @@
+// UNARY OPERATOR(+) EXAMPLES
+
+// null returns 0
+console.log(+null);
+
+// undefined returns NaN
+console.log(+undefined);
+
+// booleans: true evaluates to 1, false to 0
+console.log(+true);
+console.log(+false);
+
+// string value is evaluated and corced
+console.log(+"123");
+
+console.log(+"");
+console.log(+" ");
